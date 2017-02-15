1ecd624c2a5de05be962402591dd332beda7c49d
Shubham Agarwal <shubh.mischieveous@gmail.com>
2017-02-15 23:09:52 +0530

    Add files via upload

---
+++ newjavascript.js
@@ -0,0 +1,36 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function PurpleClick()
+{
+    document.getElementById("ColorDiv").style.backgroundColor="purple";
+}
+
+function RedClick()
+{
+    document.getElementById("ColorDiv").style.backgroundColor="red";
+}
+
+function BlueClick()
+{
+    document.getElementById("ColorDiv").style.backgroundColor="blue";
+}
+
+
+function YellowClick()
+{
+    document.getElementById("ColorDiv").style.backgroundColor="yellow";
+}
+
+function changeColor(color)
+{
+    document.getElementById("ColorDiv").style.backgroundColor=color;
+}
+function changeColorEvent(event)
+{
+    var x=event.srcElement;
+    document.getElementById("ColorDiv").style.backgroundColor=x.innerHTML.toLowerCase();
+}
